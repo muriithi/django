a1c8e569a51d68495a16c5d347084502f2f08b4b
muriithi <muriithik@gmail.com>
2012-03-26 12:48:35 +0300

    Added Template

--- mabuku/urls.py
+++ mabuku/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from mabuku.views import *
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -14,4 +15,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url('^$',Home),
 )
